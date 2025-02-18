21c5f581ae29876e3c2eafe3efaa84349e4d9642
anushkaMadhushan <100923370+anushkaMadhushan@users.noreply.github.com>
2025-02-18 16:05:22 +0530

    Update index.blade.php

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,38 +1,45 @@
 @extends('panel.layouts.app')
 
 @section('content')
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
 
-@if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
+    <div class="pagetitle">
+        <h1>Students</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+                <li class="breadcrumb-item active">Students</li>
+            </ol>
+        </nav>
     </div>
-@endif
 
-<div class="pagetitle">
-    <h1>Students</h1>
-    <nav>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
-            <li class="breadcrumb-item active">Students</li>
-        </ol>
-    </nav>
-</div>
+    <div class="row mb-4">
+        <div class="col-md-6">
+            <form action="{{ route('students.index') }}" method="GET">
+                <input type="text" name="search" class="form-control" placeholder="Search by name or email" value="{{ request('search') }}">
+            </form>
+        </div>
+        <div class="col-md-6 text-end">
+            <a href="{{ route('students.create') }}" class="btn btn-primary">Add Student</a>
+        </div>
+    </div>
 
-<div class="row">
-    <div class="col-md-12">
-        <a href="{{ route('students.create') }}" class="btn btn-primary">Add Student</a>
-        <table class="table mt-4">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($students as $student)
+    <table class="table mt-4">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($students as $student)
                 <tr>
                     <td>{{ $loop->iteration }}</td>
                     <td>{{ $student->name }}</td>
@@ -47,10 +54,7 @@
                         </form>
                     </td>
                 </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-</div>
-
+            @endforeach
+        </tbody>
+    </table>
 @endsection
